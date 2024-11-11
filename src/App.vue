99c1d9ae4582af920dfd0620db258b5770ce1e15
dnzlucas <lucasdefenix@gmail.com>
2024-11-11 16:47:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import teste from './views/HomeView.vue'
+</script>
+
+<template>
+
+
+  <RouterView />
+</template>
+
+<style scoped></style>
